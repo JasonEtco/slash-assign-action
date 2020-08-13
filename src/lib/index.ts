e77935f549e54c8ab7e01b1c3f65abc5321ab878
Jason Etcovitch <jasonetco@github.com>
2020-08-13 16:20:54 -0400

    Fix TS

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -9,7 +9,7 @@ export interface Inputs {
   days_until_warning: string
   days_until_unassign: string
   stale_assignment_label: string
-  [key: string]: string
+  [key: string]: string | undefined
 }
 
 export type SlashAssignToolkit = Toolkit<Inputs>
